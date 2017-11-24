562f7273e07c75cdbd86ef6a97392e788d3f4be4
Aqib Abass <mir_aquib@yahoo.com>
2017-11-24 01:58:59 -0500

    Adding POST method to add todos List and refactoring code using underscorejs

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
+var _ = require('underscore');
 
+
+var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-        id: 1,
-        descriptioon: 'Get up for gym',
-        status: false
- },
-    {
-        id: 2,
-        descriptioon: 'Get to market',
-        status: false
-},
-    {
-        id: 3,
-        descriptioon: 'complete the web-app project',
-        status: true
+var todos = []
+var todoNextId = 1;
 
-}]
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('To Do api Root!');
@@ -29,23 +20,31 @@ app.get('/todos', (req, res) => {
 });
 
 app.get(`/todos/:id`, (req, res) => {
+    
     var todoId = parseInt(req.params.id, 10);
-    console.log(todoId);
-    var matchedTodo;
-    
-    todos.forEach(function (todo) {
-        if(todoId === todo.id){
-            matchedTodo = todo;
-        }
-    });
-    
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+   
     if(matchedTodo) {
         res.json(matchedTodo);
     }
     else {
         res.status(404).send();
     }
-})
+});
+
+app.post('/todos', (req, res) => {
+    var body = _.pick(req.body, 'description','completed','id');
+    
+    if(!_.isBoolean(body.completed) ||!_.isString(body.description) || body.description.trim().length=== 0) {
+        return res.status(400).send();
+    }
+    
+    body.description = body.description.trim();
+    body.id = todoNextId++;
+    todos.push(body);
+    res.json(body);
+    
+});
 
 app.listen(PORT, function() {
     console.log("TODO Express app is running on Port: " + PORT);
