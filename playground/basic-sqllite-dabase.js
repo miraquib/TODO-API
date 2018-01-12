c2f33d9326be78294eace9f7169e31e1b1b34ca5
mir_aquib <mir_aquib@yahoo.com>
2018-01-11 20:11:30 -0600

    Added sequelize ORM and sqlite database configurations

---
+++ playground/basic-sqllite-dabase.js
@@ -0,0 +1,54 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo', {
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 256]
+        }
+
+    },
+    completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+});
+
+sequelize.sync(
+    {force: true}
+).then(function(){
+    console.log("Everything is Synced");
+
+    Todo.create({
+        description: 'Walk the Dog',
+    }).then(function(todo){
+
+        return Todo.create({
+            description:'Clean the Office'
+        })
+    }).then(function(todo){
+        return Todo.findAll({
+            where: {
+                description: {
+                    $like: "%clean%"
+                }
+            }
+        })
+    }).then(function(todos){
+        if(todos) {
+            todos.forEach(function(todo){
+                console.log(todo.toJSON());
+            })
+        } else {
+            console.log("No TODO items")
+        }
+    }).catch(function(e){
+        console.log(e);
+    });
+});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./database.js');
 
 
 var app = express();
@@ -54,14 +55,24 @@ app.get(`/todos/:id`, (req, res) => {
 app.post('/todos', (req, res) => {
     var body = _.pick(req.body, 'description','completed','id');
     
-    if(!_.isBoolean(body.completed) ||!_.isString(body.description) || body.description.trim().length=== 0) {
-        return res.status(400).send();
-    }
+    // call db.todo
+    //  respond with 200 and todo
+    //  or else respond 404 and error message
+
+    db.todo.create(body).then(function (todo){
+        res.json(todo.toJSON());
+    }, function(e) {
+        res.status(404).json(e);
+    });
     
-    body.description = body.description.trim();
-    body.id = todoNextId++;
-    todos.push(body);
-    res.json(body);
+    // if(!_.isBoolean(body.completed) ||!_.isString(body.description) || body.description.trim().length=== 0) {
+    //     return res.status(400).send();
+    // }
+    
+    // body.description = body.description.trim();
+    // body.id = todoNextId++;
+    // todos.push(body);
+    // res.json(body);
     
 });
 
@@ -104,6 +115,8 @@ app.put('/todos/:id', (req, res) => {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-    console.log("TODO Express app is running on Port: " + PORT);
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function() {
+        console.log("TODO Express app is running on Port: " + PORT);
+    });
 });
